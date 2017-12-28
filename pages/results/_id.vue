fa5c3845fca3aad8c30f0e216d5d9bf9be371f50
VranceanuM <markdevstudio@gmail.com>
2017-12-28 21:41:51 +0200

    final step

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,12 +1,51 @@
 <template>
   <div>
-      <h1>Hello{{$route.params.id}}</h1>
-      {{$store.state.albums}}
+      <h1>Results for {{$route.params.id}}</h1>
+
+    <div v-if="albumExists">
+      <div v-for="(album, index) in albumData">
+        <Cards
+         :title="album.collectionCensoredName"
+         :image="album.artworkUrl100"
+         :artistName="album.artistName"
+         :url="album.artistViewUrl"
+         :color="picker(index)"
+        />
+      </div>
+
+         <!--{{albumData}}-->
+     </div>
+     <div v-else>
+       <h4>Can't find album</h4>
+     </div>
   </div>
 </template>
-<script>
-  export default {
-    middleware:'search'
 
+<script>
+  import axios from 'axios'
+  import Cards from '~/components/Cards.vue'
+
+
+  export default {
+    asyncData ({params}) {
+      return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+        .then((response) => {
+            return {albumData: response.data.results}
+        })
+    },
+    middleware: 'search',
+    computed: {
+      albumExists () {
+        return this.albumData.length > 0
+      }
+    },
+    methods:{
+      picker(index){
+        return index % 2 == 0 ? 'red' : 'blue'
+      }
+    },
+    components: {
+      Cards
+    }
   }
 </script>
